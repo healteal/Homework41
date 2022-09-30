882e4408c20e17b6248f3c07b6e7792071c68940
healteal <kirylshuppa@gmail.com>
2022-09-30 15:26:40 +0300

    Added simple solution

--- src/General.java
+++ src/General.java
@@ -3,14 +3,28 @@ import java.util.regex.Pattern;
 
 public class General {
     public static void main(String[] args) {
-        System.out.println(isGoodPW("AaBbCc12"));
+        System.out.println(isGoodPW("1AaBbCc12"));
         System.out.println(isGoodPW("1aA"));
         System.out.println(isGoodPW("AAAaaaAAA"));
+        System.out.println(oneMoreSolution("1AaBbCc12"));
+        System.out.println(oneMoreSolution("1aA"));
+        System.out.println(oneMoreSolution("AAAaaaAAA"));
+
     }
 
     private static boolean isGoodPW(String password) {
         final Pattern pattern = Pattern.compile("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,100}");
         Matcher matcher = pattern.matcher(password);
         return matcher.matches();
+    }
+
+    private static boolean oneMoreSolution(String password) {
+        final Pattern patternNumbers = Pattern.compile(".*\\d.*.{8,100}");
+        final Pattern patternUpperLetters = Pattern.compile(".*[A-Z].*");
+        final Pattern patternLowerLetters = Pattern.compile(".*[a-z].*");
+        Matcher matcherNumbers = patternNumbers.matcher(password);
+        Matcher matcherUpperLetters = patternUpperLetters.matcher(password);
+        Matcher matcherLowerLetters = patternLowerLetters.matcher(password);
+        return matcherLowerLetters.matches() && matcherUpperLetters.matches() && matcherNumbers.matches();
     }
 }
